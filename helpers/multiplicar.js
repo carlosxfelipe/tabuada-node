f0a1a8aafbf1740669e16ec96568a01c39a0e110
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2022-09-08 15:27:22 -0300

    fixing color issue

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -5,22 +5,29 @@ const colors = require("colors");
 const addLeadingzero = (num) => (num >= 10 ? num : `0${num}`);
 
 const createATextFile = async (
-  multiplicador = 5,
+  multiplicador = 1,
   listar = false,
   multiplicando = 10
 ) => {
   try {
     let output = "";
+    let outputConsole = "";
 
     for (let i = 1; i <= multiplicando; i++) {
       output += `${addLeadingzero(multiplicador)} x ${addLeadingzero(
         i
       )} = ${addLeadingzero(multiplicador * i)}\n`;
+
+      outputConsole += `${addLeadingzero(multiplicador)} ${
+        "x".yellow
+      } ${addLeadingzero(i)} ${"=".yellow} ${addLeadingzero(
+        multiplicador * i
+      )}\n`;
     }
 
     if (listar) {
-      console.log(`${"Tabuada de".yellow} ${colors.yellow(multiplicador)}`);
-      console.log(output);
+      console.log(`${"Tabuada de".blue} ${colors.blue(multiplicador)}`);
+      console.log(outputConsole);
     }
 
     fs.writeFileSync(`tabuada-de-${multiplicador}.txt`, output);
